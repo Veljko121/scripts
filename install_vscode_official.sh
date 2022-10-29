21e5345f2ef5a49ca0abe585fc8046d2aea31f3d
Veljko Nikolic <nikolicveljko01@gmail.com>
2022-10-29 13:10:36 +0200

    Fixed install_vscode scripts

--- install_vscode_official.sh
+++ install_vscode_official.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install gnupg2 software-properties-common apt-transport-https curl
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
 sudo apt update
 sudo apt install code
