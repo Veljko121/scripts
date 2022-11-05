1ade0ce96d7696a4e69ee7a97ce1ea0ab85fa0a2
Veljko Nikolic <nikolicveljko01@gmail.com>
2022-11-05 21:56:31 +0100

    Added .gitignore

--- install_vscode_working.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script installs Visual Studio Code
-# according to https://linuxize.com/post/how-to-install-visual-studio-code-on-debian-10/
-
-sudo apt update
-sudo apt install gnupg2 software-properties-common apt-transport-https curl
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update
-sudo apt install code
