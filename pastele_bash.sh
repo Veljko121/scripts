51761b696f272c9270f49c04f100eb37b8aab2fd
Veljko Nikolic <nikolicveljko01@gmail.com>
2022-12-09 16:34:07 +0100

    Added a command to create the .config This command creates the folder .config if it doesn't already exist.

--- pastele_bash.sh
+++ pastele_bash.sh
@@ -2,6 +2,7 @@
 # This script changes bash to starship pastele preset
 
 sudo apt install curl -y
+mdkir -p ~/.config
 curl -sS https://starship.rs/install.sh | sh
 starship preset pastel-powerline > ~/.config/starship.toml
 python3 python_scripts/add_starship.py
