f0222bb8f6f1e672784a680c4767394f6bd81551
Veljko Nikolic <nikolicveljko01@gmail.com>
2022-12-09 14:28:01 +0100

    A few fixes

--- pastele_bash.sh
+++ pastele_bash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script changes bash to starship pastele preset
 
+sudo apt install curl
 curl -sS https://starship.rs/install.sh | sh
 starship preset pastel-powerline > ~/.config/starship.toml
 python3 python_scripts/add_starship.py
