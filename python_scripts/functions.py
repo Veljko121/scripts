da2f7a0c044b9694d9972ad378342ef1288edc40
Veljko Nikolic <nikolicveljko01@gmail.com>
2022-12-09 14:32:23 +0100

    Small fix

--- python_scripts/functions.py
+++ python_scripts/functions.py
@@ -3,7 +3,7 @@ import os
 
 def get_username():
     filename = "username.txt"
-    os.system(f"whoami >> {filename}")
+    os.system("whoami >> " + filename)
     with open(filename, 'r') as file:
         username = file.read().strip()
     os.remove(filename)
