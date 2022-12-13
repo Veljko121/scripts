2a38e37bab0a81cb2be5222dd3f9945f306f21b9
Veljko Nikolic <nikolicveljko01@gmail.com>
2022-12-13 10:50:53 +0100

    Added sudo for the sed command

--- change_hostname.sh
+++ change_hostname.sh
@@ -8,7 +8,7 @@ read -p "Enter new hostname: " hostname
 sudo hostnamectl set-hostname $hostname
 
 # Update the /etc/hosts file
-sed -i "s/127.0.1.1.*/127.0.1.1 $hostname/g" /etc/hosts
+sudo sed -i "s/127.0.1.1.*/127.0.1.1 $hostname/g" /etc/hosts
 
 # Print the new hostname
 echo "The new hostname is: $hostname"
