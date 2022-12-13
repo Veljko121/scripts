453a37a0dcae1d34523717da9678cf3a7b932475
Veljko Nikolic <nikolicveljko01@gmail.com>
2022-12-13 11:57:25 +0100

    Updated change_hostname.sh

--- change_hostname.sh
+++ change_hostname.sh
@@ -2,13 +2,22 @@
 # This script changes the hostname.
 
 # Prompt user for new hostname
-read -p "Enter new hostname: " hostname
+read -p "Enter new hostname: " new_hostname
+
+# Set the hostname file
+hostname_file="/etc/hostname"
+
+# Set the hosts file
+hosts_file="/etc/hosts"
+
+# Set the current hostname
+current_hostname=$(hostname)
+
+# Replace the current hostname with the new hostname in the hostname file
+sudo sed -i "s/$current_hostname/$new_hostname/g" $hostname_file
+
+# Replace the current hostname with the new hostname in the hosts file
+sudo sed -i "s/$current_hostname/$new_hostname/g" $hosts_file
 
 # Set the new hostname
-sudo hostnamectl set-hostname $hostname
-
-# Update the /etc/hosts file
-sudo sed -i "s/127.0.1.1.*/127.0.1.1 $hostname/g" /etc/hosts
-
-# Print the new hostname
-echo "The new hostname is: $hostname"
+sudo hostname $new_hostname
