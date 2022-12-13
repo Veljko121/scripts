93bbc11bb9ca2790f0ab7c2e25e851ad57372030
Veljko Nikolic <nikolicveljko01@gmail.com>
2022-12-13 10:51:28 +0100

    Updated change_grub.sh Removed change_grub.py

--- change_grub.sh
+++ change_grub.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # This script changes grub to not wait 5s
 
-python3 python_scripts/change_grub.py
+# Open the grub configuration file in a text editor
+sudo sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
+
+# Update the grub configuration
+sudo update-grub
