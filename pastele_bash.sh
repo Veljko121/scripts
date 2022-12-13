560eff6e4de0cafefe651891f76399b0f1c1fa3e
Veljko Nikolic <nikolicveljko01@gmail.com>
2022-12-13 11:31:38 +0100

    Updated pastele_bash.sh Removed add_starship.py

--- pastele_bash.sh
+++ pastele_bash.sh
@@ -5,8 +5,14 @@ sudo apt install curl -y
 mdkir -p ~/.config
 curl -sS https://starship.rs/install.sh | sh
 starship preset pastel-powerline > ~/.config/starship.toml
-python3 python_scripts/add_starship.py
 
-echo "You can change the color preset by running the command:"
-echo
-echo "python3 python_scripts/palettes.py python_scripts/palettes/<color>.txt"
+file="/home/$USER/.bashrc"
+line="eval \"\$(starship init bash)\""
+
+if ! grep -Fxq "$line" $file; then
+  # If the line is not found, append it to the file
+  echo $line >> $file
+fi
+
+echo "You can change the color preset by running the following command:"
+echo -e "\033[32mpython3 python_scripts/palettes.py python_scripts/palettes/<color>.txt\033[0m"
