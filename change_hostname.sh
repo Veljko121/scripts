c16895fa6975d7b6d2945d6382e2829787c604db
Veljko Nikolic <nikolicveljko01@gmail.com>
2022-12-14 13:52:15 +0100

    Updated change_hostname.sh

--- change_hostname.sh
+++ change_hostname.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 # This script changes the hostname.
 
+# Set the hostname file and the hosts file
+hostname_file="hostname"
+hosts_file="hosts"
+
 # Prompt user for new hostname
 read -p "Enter new hostname: " new_hostname
 
-# Set the hostname file
-hostname_file="/etc/hostname"
+# Get the current hostname
+read -r current_hostname < <(cat "/etc/$hostname_file")
 
-# Set the hosts file
-hosts_file="/etc/hosts"
+# Copy files to the home directory
+cp "/etc/$hostname_file" ~
+cp "/etc/$hosts_file" ~
 
-# Set the current hostname
-current_hostname=$(hostname)
+# Replace the specified string in the copied file using sed
+sed -i "s/$current_hostname/$new_hostname/g" ~/"$hostname_file"
 
-# Replace the current hostname with the new hostname in the hostname file
-sudo sed -i "s/$current_hostname/$new_hostname/g" $hostname_file
+# Replace the specified string in the copied file using sed
+sed -i "s/$current_hostname/$new_hostname/g" ~/"$hosts_file"
 
-# Replace the current hostname with the new hostname in the hosts file
-sudo sed -i "s/$current_hostname/$new_hostname/g" $hosts_file
+# Replace the old file with the new one
+sudo mv ~/"$hostname_file" "/etc/$hostname_file"
+sudo mv ~/"$hosts_file" "/etc/$hosts_file"
 
-# Set the new hostname
-sudo hostname $new_hostname
+echo "Hostname successfully changed. To save the changes reboot the system."
