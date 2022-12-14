0d06d808370191fcde8da39a9073fb5e79a16521
Veljko Nikolic <nikolicveljko01@gmail.com>
2022-12-14 13:56:49 +0100

    Automatically installs Eclipse

---
+++ install_eclipse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script installs Eclipse for Java development on a Linux system
+
+# Update the package index
+sudo apt update
+
+# Install the Eclipse package
+sudo apt install eclipse-platform
+
+# Install the Java Development Kit (JDK)
+sudo apt install default-jdk
